af87644f85dc1e8c280172492d844cd274acea66
whosjane <jane85017503@gmail.com>
2025-02-20 18:42:37 +0900

    button

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from "react";
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>hi</Text>
+      <View style={{ flexDirection: "row", gap: 10 }}>
+        <Button title= "패션" 
+                color= "#000000"/>
+        <Button title= "뷰티"
+                color= "#000000" />
+        <Button title= "액세서리" 
+                color= "#000000"/>
+        <Button title= "라이프"
+                color= "#000000" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
